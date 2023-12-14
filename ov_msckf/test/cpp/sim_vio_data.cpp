80b49b8fa08e2d7daabb2be77465f6fabff24e48
Lin Zhao <linzhao@uri.edu>
2023-12-14 11:16:42 -0500

    finish vio_sim

--- ov_msckf/test/cpp/sim_vio_data.cpp
+++ ov_msckf/test/cpp/sim_vio_data.cpp
@@ -36,7 +36,10 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/Vector3Stamped.h>
-
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/ChannelFloat32.h>
+#include <geometry_msgs/Point32.h>
 #endif
 
 #include "core/VioManagerOptions.h"
@@ -111,7 +114,7 @@ int main(int argc, char **argv) {
       poseIinG.pose.position.x = state_gt(5, 0);
       poseIinG.pose.position.y = state_gt(6, 0);
       poseIinG.pose.position.z = state_gt(7, 0);
-      bag.write("/ov_msckf/sim_sth/state_gt/pose", ros::Time(state_gt(0, 0)), poseIinG);
+      bag.write("/ov_msckf/state_gt/pose", ros::Time(state_gt(0, 0)), poseIinG);
 
       // save v
       geometry_msgs::TwistStamped velocityIinG;
@@ -120,7 +123,7 @@ int main(int argc, char **argv) {
       velocityIinG.twist.linear.x = state_gt(8, 0);      
       velocityIinG.twist.linear.y = state_gt(9, 0);      
       velocityIinG.twist.linear.z = state_gt(10, 0);  
-      bag.write("/ov_msckf/sim_sth/state_gt/velocity", ros::Time(state_gt(0, 0)), velocityIinG);
+      bag.write("/ov_msckf/state_gt/velocity", ros::Time(state_gt(0, 0)), velocityIinG);
 
       // save gyro bias 
       geometry_msgs::Vector3Stamped bg;
@@ -129,7 +132,7 @@ int main(int argc, char **argv) {
       bg.vector.x = state_gt(11, 0);
       bg.vector.y = state_gt(12, 0);
       bg.vector.z = state_gt(13, 0);
-      bag.write("/ov_msckf/sim_sth/state_gt/bg", ros::Time(state_gt(0, 0)), bg);
+      bag.write("/ov_msckf/state_gt/bg", ros::Time(state_gt(0, 0)), bg);
 
       // save accl bias 
       geometry_msgs::Vector3Stamped ba;
@@ -138,7 +141,7 @@ int main(int argc, char **argv) {
       ba.vector.x = state_gt(14, 0);
       ba.vector.y = state_gt(15, 0);
       ba.vector.z = state_gt(16, 0);
-      bag.write("/ov_msckf/sim_sth/state_gt/ba", ros::Time(state_gt(0, 0)), ba);      
+      bag.write("/ov_msckf/state_gt/ba", ros::Time(state_gt(0, 0)), ba);      
     }
 
     // IMU: get the next simulated IMU measurement if we have it
@@ -147,6 +150,18 @@ int main(int argc, char **argv) {
     bool hasimu = sim.get_next_imu(time_imu, wm, am);
     if (hasimu) {
       PRINT_DEBUG("new imu measurement = %0.15g | w = %0.3g | a = %0.3g\n", time_imu, wm.norm(), am.norm());
+
+      // save to rosbag
+      sensor_msgs::Imu msg_imu;
+      msg_imu.header.stamp = ros::Time(time_imu);
+      msg_imu.header.frame_id = "imu";
+      msg_imu.linear_acceleration.x = am(0);
+      msg_imu.linear_acceleration.y = am(1);
+      msg_imu.linear_acceleration.z = am(2);
+      msg_imu.angular_velocity.x = wm(0);
+      msg_imu.angular_velocity.y = wm(1);
+      msg_imu.angular_velocity.z = wm(2);
+      bag.write("/ov_msckf/sim/imu", ros::Time(time_imu), msg_imu);
     }
 
     // CAM: get the next simulated camera uv measurements if we have them
@@ -156,6 +171,33 @@ int main(int argc, char **argv) {
     bool hascam = sim.get_next_cam(time_cam, camids, feats);
     if (hascam) {
       PRINT_DEBUG("new cam measurement = %0.15g | %u cameras | uvs(0) = %u \n", time_cam, camids.size(), feats.at(0).size());
+
+      // save to bag
+      for(size_t i = 0; i <feats.size(); i++) {
+
+        sensor_msgs::PointCloud::Ptr cam_uvs(new sensor_msgs::PointCloud);
+        sensor_msgs::ChannelFloat32 cam_ids;
+
+        for(const auto& feat : feats.at(i)) {
+          geometry_msgs::Point32 measurement;
+          // u
+          measurement.x = feat.second(0);
+          // v
+          measurement.y = feat.second(1);
+          // 1
+          measurement.z = 1.0;
+
+          cam_uvs->points.push_back(measurement);
+          cam_ids.values.push_back(feat.first);
+        }
+
+        std::string ros_topic = "/ov_msckf/sim/cam_uvs" + std::to_string(i);
+        cam_uvs->channels.push_back(cam_ids);
+        bag.write(ros_topic, ros::Time(time_cam), cam_uvs);
+      }
+
+
+
     }
   }
 
